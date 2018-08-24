42b7ee66361cef6fbd6f82126c117afdd821b693
Anas Gauba <anas.farooq71@gmail.com>
2018-08-24 12:56:20 -0600

    Created coordinates for the mouse, keeping track of mouse movement

--- src/Mouse.java
+++ src/Mouse.java
@@ -1,6 +1,38 @@
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
 /**
+ * Created a x and a y position for the mouse.
+ * Added a MouseEvent to make sure the mouse moves
+ * and gets the x and y coordinates.
  * @version date: 2018-08-24
  * @author Anas Farooq Gauba
  */
 public class Mouse {
+    private double x;
+    private double y;
+
+    /**
+     * Keeps track of the mouse position using EventHandler.
+     * @return the mouse position
+     */
+    public EventHandler<MouseEvent> getMouseMovedHandler () {
+        return event -> {
+            x = event.getX ();
+            y = event.getY ();
+        };
+    }
+
+    /**
+     * @return x position of the mouse.
+     */
+    public double getX () {
+        return x;
+    }
+    /**
+     * @return y position of the mouse.
+     */
+    public double getY () {
+        return y;
+    }
 }
--- src/PinBallGame.java
+++ src/PinBallGame.java
@@ -1,3 +1,6 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * PinBall game cooridnator class. A parent class
  * that has access of its children classes such as
@@ -5,9 +8,15 @@
  * @version date: 2018-08-24
  * @author Anas Farooq Gauba
  */
-public class PinBallGame {
+public class PinBallGame extends Application {
 
     public static void main(String[]args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle ("PinBall Machine Game");
+        primaryStage.show();
     }
 }
